aa54845efc6d831b23af2d8123710443bb8be50c
itsSoheil <soheil.samadi79@gmail.com>
2024-02-24 08:24:05 +0330

    Use UsersList

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,14 @@ import "./App.css";
 import Cake from "./components/Cake";
 import { myStore } from "./Redux/store";
 import Milk from "./components/Milk";
+import UsersList from "./components/UsersList";
 function App() {
   return (
     <Provider store={myStore}>
       <Cake />
       <Milk />
+      <hr />
+      <UsersList />
     </Provider>
   );
 }
